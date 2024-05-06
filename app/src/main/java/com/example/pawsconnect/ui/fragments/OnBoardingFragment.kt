9020f2c25b4c627e56947a5e656f1469d7baa2ca
dsds <dsds@gmail.com>
2024-05-06 15:52:02 +0400

    Initial commit

---
+++ app/src/main/java/com/example/pawsconnect/ui/fragments/OnBoardingFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pawsconnect.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pawsconnect.R
+import com.example.pawsconnect.databinding.FragmentHomeBinding
+import com.example.pawsconnect.databinding.FragmentOnBoardingBinding
+
+class OnBoardingFragment : Fragment() {
+
+    private var _binding: FragmentOnBoardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
