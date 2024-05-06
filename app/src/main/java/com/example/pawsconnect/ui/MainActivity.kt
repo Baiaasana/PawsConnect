d4f4770f2817def2d8bbec06966a2b255a7952f1
dsds <dsds@gmail.com>
2024-05-06 16:46:15 +0400

    set up navigation

--- app/src/main/java/com/example/pawsconnect/ui/MainActivity.kt
+++ app/src/main/java/com/example/pawsconnect/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.pawsconnect
+package com.example.pawsconnect.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pawsconnect.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
